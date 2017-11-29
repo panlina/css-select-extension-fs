3525ef2711c4cc6c4410dfd07c3901dc03349090
Jack Lu <jacklu@jacklu.me>
2017-11-29 23:49:52 +0800

    +create-time, modify-time

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var falseFunc = require('boolbase').falseFunc;
+var attribute = {
+	'size': 'byte',
+	'create-time': 'date',
+	'modify-time': 'date'
+};
 module.exports = function (what, select) {
 	what.actionTypes['<'] = 'lt';
 	what.actionTypes['>'] = 'gt';
@@ -7,10 +12,10 @@ module.exports = function (what, select) {
 		return function (next, data) {
 			var name = data.name,
 				value = data.value;
-			
+
 			return function start(elem) {
 				var attr = adapter.getAttributeValue(elem, name);
-				return attr != null && attr <= parseSize(value) && next(elem);
+				return attr != null && attr <= parse(attribute[name], value) && next(elem);
 			};
 		};
 	};
@@ -18,13 +23,19 @@ module.exports = function (what, select) {
 		return function (next, data) {
 			var name = data.name,
 				value = data.value;
-			
+
 			return function start(elem) {
 				var attr = adapter.getAttributeValue(elem, name);
-				return attr != null && attr >= parseSize(value) && next(elem);
+				return attr != null && attr >= parse(attribute[name], value) && next(elem);
 			};
 		};
 	};
+	function parse(type, value) {
+		switch (type) {
+			case 'byte': return parseSize(value);
+			case 'date': return parseDate(value);
+		}
+	}
 	function parseSize(value) {
 		var match = value.match(/([0-9]+)(k|m|g)?/);
 		var value = parseInt(match[1]), unit = match[2];
@@ -34,5 +45,8 @@ module.exports = function (what, select) {
 			case 'g': value <<= 30; break;
 		}
 		return value;
+	}
+	function parseDate(value) {
+		return new Date(value).valueOf();
 	}
 };
